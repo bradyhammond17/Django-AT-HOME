798cc82a91e819cc801ebd042b14bdef346162c5
Brady Hammond <brady_hammond1.baylor.edu>
2022-05-02 09:04:14 -0500

    Caught up to 4/27 class

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TopicForm
 from .models import Topic
 # Create your views here.
 
@@ -12,4 +14,27 @@ def topics(request):
 
     context = {'topics': topics}
 
-    return render(request, 'MainApp/topics.html', context) 
+
+    return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+    ## view must be consistent with url
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic':topic,'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            new_topic = form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context) 
